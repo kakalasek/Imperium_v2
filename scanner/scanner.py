17eae9cc04ba5a7ee75fe87d77fdfb7643bd521b
Kakalasek <honziccekkj@seznam.cz>
2024-08-04 11:49:44 +0200

    creating the scan database

--- scanner/scanner.py
+++ scanner/scanner.py
@@ -1,10 +1,14 @@
 from flask import Flask, request
+from models import db
 import subprocess
 import json
 import xmltodict
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "73eeac3fa1a0ce48f381ca1e6d71f077"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/pipa/Personal/Projects/Imperium/v2/controller/instance/db.sqlite3'
+db.init_app(app)
+
 
 def createJson():
     xml_content = subprocess.getoutput(f"sudo nmap -oX - {request.args.get('options')} {request.args.get('range')}")
@@ -24,4 +28,6 @@ def scan():
     return json_output
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, port=3001)
